dc66f8023659a0b1396ce572505105291c6adb72
Yuriy Gyerts <gyerts@gmail.com>
2017-11-22 00:41:40 +0200

    ignore products folder

--- 4.2.Enumeration/wdcount.cpp
+++ 4.2.Enumeration/wdcount.cpp
@@ -1,27 +1,33 @@
 #include <iostream>
 #include <string>
-using namespace std;
+
 
 enum class NOORYES {
 	NO = 0,
 	YES = 1
 };
 
-int main()
-{
-		
-	string a;
-	int i = 0;
-	cout << "Enter: " << endl;
-	getline(cin, a);
-	for(int i = 0; i < a.size(); i++) {
-		if(a[i] == ' ') {
-			cout << "It is a space!!!" << endl;
+int main() {
+	std::string user_input;
+	std::cout << "Enter: " << std::endl;
+	getline(std::cin, user_input);
+	for(int i = 0; i < user_input.size(); i++) {
+		if(user_input[i] == ' ') {
+			std::cout << "It is user_input space!!!" << std::endl;
 		}
-		else(a[i] != ' '); {
-			cout << "Not space." << i;
+		else {
+			std::cout << "Not space." << i;
 		}
 	}
 	
-return 0;	
+	return 0;	
 }
+
+// for(блок1;блок2;блок3) {тело;}
+// блок1 - обычно объявление переменных
+// блок2 - условие при котором будет работать цикл
+// блок3 - пост действие итерации
+
+// смещение == i
+// 0   1   2   3   4   5   6   7   8   9
+// | a | b | c |   | a | b | c |   |   | a | = 10
