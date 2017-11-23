10b0e705255a99ccdbe29ef278e43612bf27461a
Yuriy Gyerts <gyerts@gmail.com>
2017-11-23 12:48:34 +0200

    the end of enum code

--- 4.2.Enumeration/wdcount.cpp
+++ 4.2.Enumeration/wdcount.cpp
@@ -2,26 +2,37 @@
 #include <string>
 
 
-enum class NOORYES {
+enum class WORD {
 	NO = 0,
 	YES = 1
 };
 
 int main() {
+	WORD isWord = WORD::NO;
+	
+	int wordcount = 0;
+	
 	std::string user_input;
 	std::cout << "Enter: " << std::endl;
 	getline(std::cin, user_input);
+
 	for(int i = 0; i < user_input.size(); i++) {
-		if(user_input[i] == ' ') {
-			std::cout << "It is user_input space!!!" << std::endl;
+		if (user_input[i] == ' ') {
+			isWord = WORD::NO;
+		} else {
+			if (isWord == WORD::NO)
+			{
+				wordcount++;
+				isWord = WORD::YES;
+			}
 		}
-		else {
-			std::cout << "Not space." << i;
-		}
+	
 	}
 	
+	std::cout << "Words count: " << wordcount;
+	
 	return 0;	
-}
+} 
 
 // for(блок1;блок2;блок3) {тело;}
 // блок1 - обычно объявление переменных
