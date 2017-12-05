1b7174e5d194253975807e319d75313e7ee1ec21
Yuriy Gyerts <gyerts@gmail.com>
2017-12-05 18:37:20 +0200

    compiled error

--- 4.DZ/date.hpp
+++ 4.DZ/date.hpp
@@ -12,11 +12,11 @@ void run() {
 	Date date;
 	char slash = '/';
 	std::cout << "Enter the date in the format day/month/year: " << std::endl;
-	std::cin >> date.day >> slash >> date.month >> slash >> date.year;
-	if (date.day < 31 && date.month < 13) {
+	std::cin >> date.day >> date.month >> date.year;
+	if (date.day > 31 || date.day <= 0 || date.month > 12 || date.month <= 0 || date.year < 0) {
+		std::cout << "Format was entered incorrectly." << std::endl;	
+	} else (date.day < 31 && date.month < 13) {
 		std::cout << "Today " << date.day << slash << date.month << slash << date.year << std::endl;
-	} else if (date.day > 31 || date.month > 12) {
-		std::cout << "Format was entered incorrectly." << std::endl;
-	}
+	}	
 }
 }
